a31c91311d6247e613af2729b4a1da6ff00bc62e
Дмитрий <dimichru@yandex.ru>
2023-01-28 03:14:45 +0500

    доделал стили для кнопок, переход на 3ю страницу со входа + поменял textview на button "перейти в блог"

--- app/src/main/java/com/example/askme_app/MainActivity.java
+++ app/src/main/java/com/example/askme_app/MainActivity.java
@@ -14,11 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button2);
+        Button btnEnter = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent switcher = new Intent(MainActivity.this, activity_register.class);
                 MainActivity.this.startActivity(switcher);
+            }
+        });
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, activity_top3_authors.class));
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/askme_app/activity_top3_authors.java
+++ app/src/main/java/com/example/askme_app/activity_top3_authors.java
@@ -3,12 +3,19 @@ package com.example.askme_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class activity_top3_authors extends AppCompatActivity {
+
+    Button btnGoBlog1, btnGoBlog2, btnGoBlog3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top3_authors);
+
+        btnGoBlog1 = findViewById(R.id.btnGoBlog1);
+        btnGoBlog2 = findViewById(R.id.btnGoBlog2);
+        btnGoBlog3 = findViewById(R.id.btnGoBlog3);
     }
 }
