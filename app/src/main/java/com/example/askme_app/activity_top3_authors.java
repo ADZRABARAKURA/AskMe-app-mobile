89534ff15385b637a7991e8414241b6456b88e8c
Дмитрий <dimichru@yandex.ru>
2023-01-28 09:54:49 +0500

    добавил переход на 4ю страницу

--- app/src/main/java/com/example/askme_app/activity_top3_authors.java
+++ app/src/main/java/com/example/askme_app/activity_top3_authors.java
@@ -2,7 +2,9 @@ package com.example.askme_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class activity_top3_authors extends AppCompatActivity {
@@ -17,5 +19,13 @@ public class activity_top3_authors extends AppCompatActivity {
         btnGoBlog1 = findViewById(R.id.btnGoBlog1);
         btnGoBlog2 = findViewById(R.id.btnGoBlog2);
         btnGoBlog3 = findViewById(R.id.btnGoBlog3);
+
+        btnGoBlog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(activity_top3_authors.this, activity_author_blog.class));
+                finish();
+            }
+        });
     }
 }
